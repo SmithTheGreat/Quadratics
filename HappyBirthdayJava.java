4c3e7e5e1c23aeb8ac9518e334a8f026ff062599
SmithTheGreat <109917315+SmithTheGreat@users.noreply.github.com>
2023-08-22 14:31:35 -0500

    Create HappyBirthdayJava.java

---
+++ HappyBirthdayJava.java
@@ -0,0 +1,25 @@
+public class HappyBirthdayJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("HHH     HHH        A         PPPPPPPPPPP  PPPPPPPPPPP       YY        YY ");
+        System.out.println("HHH     HHH       AA         PPPPPPPPPPP  PPPPPPPPPPP        YY      YY ");
+        System.out.println("HHH     HHH      AAAA        PP       PP  PP       PP         YY    YY ");
+        System.out.println("HHHHHHHHHHH     AA  AA       PP       PP  PP       PP          YY  YY  ");
+        System.out.println("HHHHHHHHHHH    AA    AA      PPPPPPPPPPP  PPPPPPPPPPP           YYY ");
+        System.out.println("HHH     HHH   AAAAAAAAAA     PPPPPPPPPPP  PPPPPPPPPPP            YY ");
+        System.out.println("HHH     HHH  AA        AA    PP           PP                   YY ");
+        System.out.println("HHH     HHH AA          AA   PP           PP                  YY ");
+
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RRRRRRRRRRRR  TTTTTTTTTTTTTT HHH       HHH  DDDDDDD         AA        YY      YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHH       HHH  DD     DD      AAAA        YY    YY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHH       HHH  DD      DD    AA  AA        YY  YY ");
+        System.out.println("BBBBBBBBBB        II       RRRRRRRRRRR         TT       HHHHHHHHHHHHH  DD       DD  AA    AA        YYY ");
+        System.out.println("BB        BB      II       RR       RR         TT       HHH       HHH  DD      DD  AAAAAAAAAA       YY ");
+        System.out.println("BB        BB      II       RR        RR        TT       HHH       HHH  DD     DD  AA        AA     YY  ");
+        System.out.println("BBBBBBBBBBB  IIIIIIIIIIIII RR         RR       TT       HHH       HHH  DDDDDDD   AA          AA   YY ");
+        
+    }
+}
+
+
